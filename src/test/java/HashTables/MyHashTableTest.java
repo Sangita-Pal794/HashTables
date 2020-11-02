558a374cc4092199457cfcfd2edb9e9397e8828d
Sangita <pal07sangita@gmail.com>
2020-11-02 11:54:07 +0530

    Added: functionality code and test cases to find frequency of words in a large string

--- src/test/java/HashTables/MyHashTableTest.java
+++ src/test/java/HashTables/MyHashTableTest.java
@@ -20,4 +20,21 @@ public class MyHashTableTest {
         System.out.println(myHashTable);
         Assert.assertEquals(2 , frequency);
     }
+	
+	@Test
+    public void givenALargeSentence_WhenWordsAreAddedToList_ShouldReturnWordParanoidFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        MyLinkedHashMap<String , Integer> myLinkedHashMap = new MyLinkedHashMap();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myLinkedHashMap.add(word , value);
+        }
+        int frequency = myLinkedHashMap.get("paranoid");
+        System.out.println(myLinkedHashMap);
+        Assert.assertEquals(3 , frequency);
+    }
 }
